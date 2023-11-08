d830c96d9d29cd3896485f2beedaf0cfcf4f25a0
Admin <>
2023-11-07 23:41:04 -0800

    empCountCommit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
 const form = document.getElementById('addForm');
-const employeeTable = document.getElementById('employees')
+const employeesTable = document.getElementById('employees')
 
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
 let employeeCount = 0;
@@ -17,7 +17,7 @@ const extension = document.querySelector('#extension').value
 const email = document.querySelector('#email').value
 const department = document.querySelector('#department').value
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
-const row = employeeTable.insertRow();
+const row = employeesTable.insertRow();
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
 const cellID = row.insertCell();
 const cellName = row.insertCell();
@@ -27,18 +27,32 @@ const cellDepartment = row.insertCell();
 const cellDelete = row.insertCell();
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
     cellID.appendChild(document.createTextNode(id));
-    cellID.appendChild(document.createTextNode(name));
-    cellID.appendChild(document.createTextNode(extension));
-    cellID.appendChild(document.createTextNode(email));
-    cellID.appendChild(document.createTextNode(department));
+    cellName.appendChild(document.createTextNode(name));
+    cellExtension.appendChild(document.createTextNode(extension));
+    cellEmail.appendChild(document.createTextNode(email));
+    cellDepartment.appendChild(document.createTextNode(department));
     // CREATE THE DELETE BUTTON
-
+const deleteButton = document.createElement('button');
+    deleteButton.className = 'btn btn-danger btn-sm delete';
+    deleteButton.appendChild(document.createTextNode('X'));
+    cellDelete.appendChild(deleteButton);
     // RESET THE FORM
-
+form.reset();
     // SET FOCUS BACK TO THE ID TEXT BOX
-
+document.querySelector('#id').focus();
     // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
-
-})
+employeeCount++;
+countOutput.textContent = employeeCount;
+});
 
 // DELETE EMPLOYEE
+employeesTable.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure you want to delte this employee?')) {
+            const row = e.target.closest('tr');
+            employeesTable.deleteRow(row.rowIndex);
+            employeeCount--;
+            countOutput.textContent = employeeCount;
+        }
+    }
+})
